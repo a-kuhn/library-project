fb98f6528f774ff51eb0bda5b2d68402822f56ef
alex k <alex.kuhn1123@gmail.com>
2021-04-07 10:11:35 -0700

    add constructor & getters/setters to LibraryCard

--- src/com/fdmgroup/library/LibraryCard.java
+++ src/com/fdmgroup/library/LibraryCard.java
@@ -1,5 +1,35 @@
 package com.fdmgroup.library;
 
+import java.util.ArrayList;
+
 public class LibraryCard {
+	private int cardNum;
+	private ArrayList<Book> borrowedBooks;
+
+	// Constructor
+	public LibraryCard(int cardNumber) {
+		cardNum = cardNumber;
+	}
+
+	// Getters & Setters
+	public int getCardNum() {
+		return cardNum;
+	}
+
+	public void setCardNum(int cardNum) {
+		this.cardNum = cardNum;
+	}
+
+	// get list all books borrowed
+	public ArrayList<Book> getBorrowedBooks() {
+		return borrowedBooks;
+	}
+
+	public void setBorrowedBooks(ArrayList<Book> borrowedBooks) {
+		this.borrowedBooks = borrowedBooks;
+	}
+
+	// get list of books borrowed + number of times borrowed
+	// need to keep track
 
 }
